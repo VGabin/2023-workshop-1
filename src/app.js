8f69e3c1a7a2b20936a50c0dcc5ed99f9b9cf9bb
unknown <wiwimarti0693@gmail.com>
2023-09-13 14:07:59 +0200

    repair file.env

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: '../.env' });
 const express = require('express');
 const { createMeeting } = require('./controllers/eventController');
 const app = express();
@@ -7,6 +7,8 @@ app.use(express.json());
 
 app.post('/create-meeting', createMeeting);
 
+
+console.log(process.env.GRAPH_API_TOKEN);
 
 app.listen(3000, () => {
     console.log('Serveur en écoute sur le port 3000');
