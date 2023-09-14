9c6341c983b5f9b43555e80d6a990723999d0376
unknown <wiwimarti0693@gmail.com>
2023-09-14 16:26:05 +0200

    update controller fonction

--- src/controllers/eventController.js
+++ src/controllers/eventController.js
@@ -1,22 +1,50 @@
 const axios = require('axios');
-const { createEvent } = require('../models/eventModel');
-
+const EventModel = require('../models/meetingSchema');
 
 
 module.exports.createMeeting = async (req, res) => {
     try {
         const token = process.env.GRAPH_API_TOKEN;
-        const { subject, body, start, end, location, attendees } = req.body;
+        const { title, organizes, guests, start_date, end_date } = req.body;
 
-        if (!subject || !body || !start || !end || !location || !attendees) {
+        if (!title || !organizes || !guests || !start_date || !end_date) {
             return res.status(400).send('Paramètres manquants');
         }
 
-        const event = createEvent(subject, body, start, end, location, attendees);
+        const newEvent = new EventModel({ title, organizes, guests, start_date, end_date });
+        await newEvent.save();
+
+        const Guests = guests.map(guest => ({
+            emailAddress: {
+                address: guest.email,  
+                name: guest.name || ''  
+            },
+            type: "required"
+        }));
+
+        const event = {
+            subject: title,
+            body: {
+                contentType: "HTML",
+                content: "",  
+            },
+            start: {
+                dateTime: start_date,
+                timeZone: "UTC"  
+            },
+            end: {
+                dateTime: end_date,
+                timeZone: "UTC"  
+            },
+            location: {
+                displayName: "",  
+            },
+        
+        };
 
         const response = await axios.post(
             'https://graph.microsoft.com/v1.0/me/events',
-            event,
+            event, 
             {
                 headers: {
                     'Authorization': `Bearer ${token}`,
@@ -25,11 +53,20 @@ module.exports.createMeeting = async (req, res) => {
                 }
             }
         );
+        const teamsLink = response.data.joinWebUrl;
 
-        res.json(response.data);
+
+      res.json({
+        data: response.data,
+      teamsLink: teamsLink
+});
+
     
 
+        res.json(response.data);
     } catch (error) {
-        res.status(500).send(error.message);
+        console.error('Erreur:', error.response ? error.response.data : error.message);
+        res.status(500).send(error.response ? error.response.data : error.message);
     }
 };
+
